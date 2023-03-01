04b932f413233c298b34a4ff503f12e97da688c6
Rocca-Francesco <roccafrance98@yahoo.it>
2023-03-01 20:41:37 +0100

    cards base

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,17 @@
+<script>
+</script>
+
+<template>
+  <header class="d-flex align-items-center">
+    <div class="container">
+      <img src="/ygoprodeck_header_logo.png" alt="">
+    </div>
+  </header>
+</template>
+
+<style lang="scss">
+header {
+  width: 100%;
+  height: 4rem;
+}
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,49 @@
+<script>
+import { store } from "../assets/store";
+
+export default {
+  data() {
+    return {
+      store,
+    }
+  },
+}
+</script>
+
+<template>
+  <main>
+    <section id="cardsList">
+      <div class="container">
+        <div class="row row-cols-5">
+          <div id="cards" class="col text-center" v-for="card in store.cards">
+            <img :src="card.card_images[0].image_url" alt="">
+            <h3 class="text-light">{{ card.name }}</h3>
+            <span>{{ card.archetype }}</span>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
+</template>
+
+<style lang="scss">
+#cardsList {
+  background-color: #d48f38;
+
+  .row {
+    background-color: white;
+  }
+}
+
+#cards {
+  background-color: #d48f38;
+
+  img {
+    width: 100%;
+  }
+
+  h3 {
+    font-size: 1rem;
+  }
+}
+</style>
